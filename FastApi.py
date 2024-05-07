399eb2fc4a16dd18682d874da8e9a6407138625f
lenovo <y949099089@gmail.com>
2024-05-07 19:08:51 +0300

    post_preprocess

--- FastApi.py
+++ FastApi.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from TextProcessing import preprocess
 
 app = FastAPI()
+
+class SearchText(BaseModel):
+    text: str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/preprocess")
-async def root():
+async def get_preprocess():
     return {"message": preprocess("Hello World is the best")}
+
+@app.post("/preprocess")
+async def post_preprocess(searchText:SearchText):
+    return {"message": preprocess(searchText.text)}
